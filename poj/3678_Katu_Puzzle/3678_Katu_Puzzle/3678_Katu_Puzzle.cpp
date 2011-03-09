c820b1e51985279ef13837e7e0f2bcc614b79b43
fancysimon <fancysimon@163.com>
2011-03-09 23:51:38 +0800

    1

---
+++ poj/3678_Katu_Puzzle/3678_Katu_Puzzle/3678_Katu_Puzzle.cpp
@@ -0,0 +1,21 @@
+// 3678_Katu_Puzzle.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#define MAX_N 1005
+
+int graph[MAX_N * 2][MAX_N * 2];
+
+int m, n;
+
+int main()
+{
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < m; i++)
+    {
+
+    }
+	return 0;
+}
+
